035f079b859834776bfecb2f3105b977df28019e
demirbileku <udemirbilek2@gmail.com>
2021-07-06 11:56:51 +0300

    commit tree cf7e32df8fd38145376fdc08c348780846486527

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,11 @@
-export function manageFriends(state, action) {
+export function manageFriends(state = {friends: []}, action) {
   // your code here
+  switch(action.type){
+    case "friends/add":
+      return {friends: [...state.friends, action.payload]}
+    case "friends/remove":
+      return {friends: [...state.friends.filter(e => e.id != action.payload)]}
+    default:
+      return {friends: [...state.friends]}
+  }
 }
--- src/managePresents.js
+++ src/managePresents.js
@@ -1,3 +1,10 @@
-export function managePresents(state, action) {
+export function managePresents(state = {numberOfPresents: 0}, action) {
   // your code here
+
+    if (action.type === "presents/increase"){
+      return {numberOfPresents: state.numberOfPresents + 1}
+    } else {
+      return {numberOfPresents: state.numberOfPresents}
+    }
+  
 }
